16c00d13611f8cde68aa022b102a82fa9865b88f
raflytch <raflyazizabdillah30@gmail.com>
2024-09-25 18:40:15 +0700

    first commit

--- scripts/sortCarByYearAscendingly.js
+++ scripts/sortCarByYearAscendingly.js
@@ -1,12 +1,22 @@
 function sortCarByYearAscendingly(cars) {
   // Sangat dianjurkan untuk console.log semua hal hehe
-  console.log(cars);
+  // console.log(cars);
 
   // Clone array untuk menghindari side-effect
   // Apa itu side effect?
   const result = [...cars];
 
   // Tulis code-mu disini
+  for (let i = 0; i < result.length; i++) {
+    for (let j = 0; j < result.length - 1; j++) {
+      // Jika tahun mobil yang sekarang lebih besar dari tahun mobil berikutnya, lakukan swap
+      if (result[j].year > result[j + 1].year) {
+        let temp = result[j];
+        result[j] = result[j + 1];
+        result[j + 1] = temp;
+      }
+    }
+  }
 
   // Rubah code ini dengan array hasil sorting secara ascending
   return result;
